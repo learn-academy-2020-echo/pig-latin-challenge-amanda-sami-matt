4c3ebc23b12ab5b9be75d8bfd576321ad20ae186
M-ram224 <mattramirez2011@gmil.com>
2020-11-17 16:41:13 -0800

    Complete pig latin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     // the state object holds information that can be displayed to the user and updated throughout the program
     this.state = {
       // "phrase" is the text entered by the user - right now there are some test words hard coded to make the process of testing your code a bit faster and easier
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
-      phrase: "echo through yummy squeal queen fry",
+      phrase: "",
+      // through yummy squeal queen fry
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: "This is where your translated sentence will appear.",
     }
   }
 
@@ -23,24 +24,70 @@ class App extends Component {
     console.log("userInput:", userInput)
 
     // now that we have an array of words, we can map over the array and access each word
-    let translatedWordsArray = userInput.map(currentWord => {
+    let translatedWordsArray = userInput.map((currentWord) => {
       // ACTION ITEM: use "currentWord" as a starting point for your code
       console.log("currentWord:", currentWord)
 
-      let vowelsArray = currentWord.split("").filter(vowel => {
-        return vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u"
+      let vowelsArray = currentWord.split("").filter((vowel) => {
+        return (
+          vowel === "a" ||
+          vowel === "e" ||
+          vowel === "i" ||
+          vowel === "o" ||
+          vowel === "u"
+        )
       })
       console.log("vowelsArray:", vowelsArray)
-      
+
       // your code here!
-
+      currentWord = currentWord.toLowerCase()
+      // check typeof "str"
+      if (typeof currentWord !== "string") {
+        alert("Enter words please")
+      } else if (currentWord[0] === vowelsArray[0]) {
+        // If 1st letter of currentWord = vowel
+        // return currentWord + (template lieral ) way
+        // console.log(currentWord)
+        return `${currentWord}way`
+      } else if (currentWord.includes("qu")) {
+        // "qu" case
+        // if currentWord .includes(qu) then find indexOf(u)
+        // var = index Of first "u"
+        let uIndex = currentWord.indexOf("u")
+        // var = return currentWord.substr(0,indexOf 1st "u")
+        let quWordPreCons = currentWord.substr(0, uIndex + 1)
+        let quWordPostCons = currentWord.substr(uIndex + 1)
+        // squ-eal = eal-squ-"ay"
+        // return remaining letters in word "eal"+ "squ" + "ay" (substr(0)) ***. length -1***
+        return `${quWordPostCons}${quWordPreCons}ay`
+      } else if (vowelsArray.length === 0) {
+        // && currentWord.includes("y")
+        // "y" case
+        // if the "str" does not contain any vowels in vowel array
+        // let vowelsInWord = vowelsArray.join
+        let yIndex = currentWord.indexOf("y")
+        console.log("ycase:", currentWord)
+        console.log("yIndex:", yIndex)
+        let yWordPreCons = currentWord.substr(0, yIndex)
+        let yWordPostCons = currentWord.substr(yIndex)
+        // return "y" + remaining letters in word + "ay" (substr(0)) ***.length -1***
+        return `${yWordPostCons}${yWordPreCons}ay`
+      } else {
+        // "consonants" case
+        // assign a variable to firstVowel
+        // var locationFirstVowel = currentWord.indexOf(firstVowel)
+        // var beginningCons = currentWord.stubstr(0,  )
+        let firstVowel = vowelsArray[0]
+        let indexOfFirstVowel = currentWord.indexOf(firstVowel)
+        let conPreCons = currentWord.substr(0, indexOfFirstVowel)
+        let conPostCons = currentWord.substr(indexOfFirstVowel)
+        return `${conPostCons}${conPreCons}ay`
+      }
       // Remember: console.log is your friend :)
-
 
       // ACTION ITEM: update the value being returned to reflect the output of your code
       return currentWord
     })
-
 
     // joining the array back to a string of translated words
     // no need to change this variable
@@ -52,13 +99,12 @@ class App extends Component {
     this.setState({ phraseTranslated: translatedWords })
   }
 
-
   restartGame = () => {
     // this method restarts the game by setting the original state
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
-      phrase: "echo through yummy squeal queen fry",
-      phraseTranslated: "This is where your translated sentence will appear."
+      phrase: "",
+      phraseTranslated: "This is where your translated sentence will appear.",
     })
   }
 
@@ -91,15 +137,15 @@ class App extends Component {
           <input
             type="text"
             id="inputPhrase"
-            onChange={ this.handleInput }
-            value={ this.state.phrase }
+            onChange={this.handleInput}
+            value={this.state.phrase}
           />
           <br />
           {/* button that called the setUpPreventDefault method which calls the myPigLatinCodeHere method */}
-          <button onClick={ this.setUpPreventDefault }>Submit</button>
-          <button onClick={ this.restartGame }>Clear</button>
+          <button onClick={this.setUpPreventDefault}>Submit</button>
+          <button onClick={this.restartGame}>Clear</button>
         </div>
-        <p>{ this.state.phraseTranslated }</p>
+        <p>{this.state.phraseTranslated}</p>
         <footer>Coded by ~your name here~</footer>
       </React.Fragment>
     )
